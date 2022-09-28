202756a775d908a7c6f5872bca3c04592cfb9e6d
alpkanege <alpkanege@alpkan-mac-mini.home>
2022-09-28 18:19:40 +0300

    secondCommit

---
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -0,0 +1,13 @@
+package classesWithAttributes;
+
+public class ProductManager {
+	public void Add(Product product) {
+		//JDBC
+		
+	}
+	
+	public void Add2(int id, String name, String description, int stockAmound, double price) {
+		
+	}
+
+}
